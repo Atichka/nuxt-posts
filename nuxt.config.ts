f6002643112872f48731cdb46bbce66b2026a190
Atichka <nosik_atichka@mail.ru>
2024-09-14 00:32:33 +0300

    fix file

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -4,7 +4,7 @@ export default defineNuxtConfig({
   compatibilityDate: '2024-04-03',
   devtools: { enabled: true },
   modules: ['@nuxtjs/tailwindcss'],
-  router: {
-    base: 'nuxt-posts'
+  app: {
+    baseURL: '/nuxt-posts/'
   }
 })
